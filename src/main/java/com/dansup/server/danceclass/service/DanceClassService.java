704eaa6bba0e222f7261f88edd1759d2ed7fbe7d
haen-su <haensuya@gmail.com>
2023-07-07 16:53:11 +0900

    fix: import 수정

--- src/main/java/com/dansup/server/danceclass/service/DanceClassService.java
+++ src/main/java/com/dansup/server/danceclass/service/DanceClassService.java
@@ -1,13 +1,9 @@
 package com.dansup.server.danceclass.service;
 
-import com.dansup.server.danceclass.domain.ClassVideo;
 import com.dansup.server.danceclass.domain.DanceClass;
-import com.dansup.server.danceclass.repository.ClassVideoRepository;
 import com.dansup.server.danceclass.repository.DanceClassRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
