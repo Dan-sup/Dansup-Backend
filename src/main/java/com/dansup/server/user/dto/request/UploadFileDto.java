11dba68885e0826898ae6322baa430b909e635ad
haen-su <haensuya@gmail.com>
2023-07-07 17:56:58 +0900

    feat: swagger api model 설정

--- src/main/java/com/dansup/server/user/dto/request/UploadFileDto.java
+++ src/main/java/com/dansup/server/user/dto/request/UploadFileDto.java
@@ -1,9 +1,13 @@
 package com.dansup.server.user.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.web.multipart.MultipartFile;
 
+@ApiModel
 public class UploadFileDto {
 
+    @ApiModelProperty(value = "업로드할 파일", example = "multipartfile")
     private MultipartFile file;
 
 }
