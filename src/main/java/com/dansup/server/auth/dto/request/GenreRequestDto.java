72c53cf0ca674851d8b2098e867c9eb031284245
haen-su <haensuya@gmail.com>
2023-07-07 17:31:29 +0900

    feat: swagger api model 설정

--- src/main/java/com/dansup/server/auth/dto/request/GenreRequestDto.java
+++ src/main/java/com/dansup/server/auth/dto/request/GenreRequestDto.java
@@ -1,7 +1,13 @@
 package com.dansup.server.auth.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
+
+
+@ApiModel
 public class GenreRequestDto {
 
+    @ApiParam(value = "댄스 장트")
     private String genre;
 
 }
