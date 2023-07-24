32af7c8ccf6dba7c5b8c3dd66876f71315e241d9
haen-su <haensuya@gmail.com>
2023-07-24 23:06:26 +0900

    feat: profile list get dto 변경

--- src/main/java/com/dansup/server/api/profile/controller/ProfileController.java
+++ src/main/java/com/dansup/server/api/profile/controller/ProfileController.java
@@ -4,17 +4,15 @@ import com.dansup.server.api.danceclass.dto.response.GetDanceClassListDto;
 import com.dansup.server.api.profile.dto.response.GetFileUrlDto;
 import com.dansup.server.api.profile.dto.response.GetPortfolioDto;
 import com.dansup.server.api.profile.dto.response.GetProfileDetailDto;
-import com.dansup.server.api.profile.dto.response.GetProfileDto;
+import com.dansup.server.api.profile.dto.response.GetProfileListDto;
 import com.dansup.server.api.profile.service.ProfileService;
 import com.dansup.server.common.response.Response;
 import com.dansup.server.common.response.ResponseCode;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -26,29 +24,17 @@ public class ProfileController {
     private final ProfileService profileService;
     @ApiOperation(value = "Get Dancer Profiles", notes = "댄서 검색 페이지에서 댄서 프로필 조회")
     @GetMapping(value = "")
-    public Response<List<GetProfileDto>> getProfileList(@RequestParam("nickname") String nickname) {
-        List<GetProfileDto> getProfileDtos = profileService.getProfileList(nickname);
+    public Response<List<GetProfileListDto>> getProfileList(@RequestParam("nickname") String nickname) {
+        List<GetProfileListDto> getProfileDtos = profileService.getProfileList(nickname);
         return Response.success(ResponseCode.SUCCESS_OK, getProfileDtos);
     }
 
     @ApiOperation(value = "Get Dancer Profile Detail", notes = "댄서 프로필 상세 조회")
     @GetMapping(value = "/{profileId}")
     public Response<GetProfileDetailDto> getProfileDetail(@PathVariable("profileId") Long profileId) {
-        GetProfileDetailDto getProfileDetailDto = profileService.getProfile(profileId);
+        GetProfileDetailDto getProfileDetailDto = profileService.getProfileDetail(profileId);
         return Response.success(ResponseCode.SUCCESS_OK, getProfileDetailDto);
     }
-
-//    @ApiOperation(value = "Get Dancer Profile Video", notes = "댄서 프로필 대표 영상 조회")
-//    @GetMapping(value = "/{profileId}/video")
-//    public ResponseEntity<GetFileUrlDto> getProfileVideo(@PathVariable("profileId") Long profileId) {
-//        return ResponseEntity.ok(new GetFileUrlDto());
-//    }
-//
-//    @ApiOperation(value = "Get Dancer Profile Image", notes = "댄서 프로필 대표 이미지 조회")
-//    @GetMapping(value = "/{profileId}/image")
-//    public ResponseEntity<GetFileUrlDto> getProfileImage(@PathVariable("profileId") Long profileId) {
-//        return ResponseEntity.ok(new GetFileUrlDto());
-//    }
 
     @ApiOperation(value = "Get Dancer Portfolio", notes = "댄서 프로필에서 포트폴리오(공연 및 활동 경력) 조회")
     @GetMapping(value = "/{profileId}/portfolio")
--- src/main/java/com/dansup/server/api/profile/dto/response/GetProfileListDto.java
+++ src/main/java/com/dansup/server/api/profile/dto/response/GetProfileListDto.java
@@ -1,7 +1,6 @@
 package com.dansup.server.api.profile.dto.response;
 
 import com.dansup.server.api.auth.dto.request.GenreRequestDto;
-import com.dansup.server.api.auth.dto.request.HashtagRequestDto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -16,13 +15,19 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetProfileDto {
+public class GetProfileListDto {
+
+    @ApiModelProperty(value = "프로필 ID", example = "23")
+    private Long profileId;
 
     @ApiModelProperty(value = "댄서명", example = "유나")
     private String nickname;
 
     @ApiModelProperty(value = "한줄 소개", example = "나는 춤이 정말 좋아")
     private String intro;
+
+    @ApiModelProperty(value = "댄서 프로필 이미지", example = "image.jpg")
+    private String profileImage;
 
     @ApiModelProperty(value = "댄스 장르 리스트")
     private List<GenreRequestDto> genres;
