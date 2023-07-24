7ee4cd74168aeb1664b8fc638e999354e462710d
haen-su <haensuya@gmail.com>
2023-07-24 21:47:14 +0900

    feat: exception log 추가

--- src/main/java/com/dansup/server/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/dansup/server/common/exception/CustomExceptionHandler.java
@@ -12,12 +12,15 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public Response<BaseException> handleBusinessException(BaseException baseException) {
+        log.error("[BaseException 발생]: {}", baseException.getMessage());
+        log.error("[BaseException 발생]: {}", baseException);
         return Response.fail(baseException.getExceptionCode());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public Response<BaseException> handleBusinessException(RuntimeException runtimeException) {
         log.error("[RuntimeException 발생]: {}", runtimeException.getMessage());
+        log.error("[RuntimeException 발생]: {}", runtimeException);
 
         return Response.fail(ResponseCode.FAIL_BAD_REQUEST);
     }
@@ -25,6 +28,7 @@ public class CustomExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Response<BaseException> handleBusinessException(Exception exception) {
         log.error("[Exception 발생]: {}", exception.getMessage());
+        log.error("[Exception 발생]: {}", exception);
 
         return Response.fail(ResponseCode.FAIL_SERVER_ERROR);
     }
